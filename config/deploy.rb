8d0e666f38dd5b315901fdb869640e690ae00ecd
James Kerr <jkerr838@gmail.com>
2013-05-12 19:23:25 -0700

    added require rvm/capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 $:.unshift('/usr/local/rvm/rubies/ruby-2.0.0-p0/bin/')
+
 require "bundler/capistrano"
+require "rvm/capistrano"
 
 set :application, "Ebulletin"
 set :repository,  "https://github.com/jkerr838/ebulletin"
