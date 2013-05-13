889d50bfcb2010dd926f4634068e814b324a25a6
James Kerr <jkerr838@gmail.com>
2013-05-12 23:09:24 -0700

    Added the table dnd back in to precompile separately.

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module Ebulletin
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += ['preview.css', 'preview_interface.css']
+    config.assets.precompile += ['preview.css', 'preview_interface.css', 'jquery.tablednd.js']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
