00f8188e6155d22e1aafb9da2b4aa882a436d7dc
James A. Kerr <jkerr838@gmail.com>
2013-05-22 17:34:18 -0700

    Squashed bugs

--- app/helpers/preview_helper.rb
+++ app/helpers/preview_helper.rb
@@ -1,12 +1,23 @@
 module PreviewHelper
 	def inline_css
+		logger.debug "I am in the Preview Helper"
+		# Get all the locals
 		@stories = Story.order(:position).all
 		@jobs 	 = Job.order(:position).all		
+		# Get the index page as a string
 		@html = render_to_string(:action => 'index', :layout => 'generate', :locals => {:stories => @stories, :jobs => @jobs, :issue => @issue})
+		# Remove the action divs
 		all_action_divs = /<div class="action.*<\/div>/
+		# Remove the head
 		@html.gsub!(all_action_divs, '')
+		head_tag = /<title.*/
+		@html.gsub!(head_tag, '')
+		# Inline the CSS
 		premailer = Premailer.new(@html, :with_html_string => true, base_url: 'http://chapmancso.com')
 		@html = premailer.to_inline_css
+		
+		
+		@html
 	end
 	def today
 		t = Time.new
